d45d33ed990b42d3870d91856574cba166cfa7bc
timlo <a0987642936@yahoo.com>
2019-11-27 14:19:54 +0800

    Finish edit and delete function, user can add the product and delete the product as they want

--- routes/products.js
+++ routes/products.js
@@ -9,8 +9,10 @@ const blobService = azureStorage.createBlobService(AZURE_CONNECTION_STRING)
 const getStream = require('into-stream')
 const containerName = 'timloimage'
 
-
+// Bring in Article Models
 let Product = require('../models/Products')
+// Bring in User Models
+let User = require('../models/Users')
 
 
 const getBlobName = originalName => {
@@ -27,9 +29,11 @@ router.get('/add', (req, res) => {
 
 // Process the add 
 router.post('/add', uploadStrategy, (req, res) => {
+
     let product = new Product()
     product.name = req.body.name
-    product.advisor = req.body.advisor
+    // product.advisor = req.body.advisor
+    product.advisor = req.user._id  // You must login, otherwise you will get 500 error
     product.description = req.body.description
 
     const blobName = getBlobName(req.file.originalname)
@@ -48,7 +52,7 @@ router.post('/add', uploadStrategy, (req, res) => {
 
 
     product.img_url = getAuthImageUrl(blobName)
-    console.log('product image url: ',product.img_url)
+    console.log('product image url: ', product.img_url)
 
     product.save(function (err) {
         if (err) {
@@ -60,18 +64,90 @@ router.post('/add', uploadStrategy, (req, res) => {
 })
 
 
+// Load edit form
+router.get('/edit/:id', (req, res) => {
+    Product.findById(req.params.id, function (err, product) {
+        res.render('edit_product', {
+            title: 'Edit Product',
+            product: product
+        })
+    })
+})
+
+// Update submit post route
+router.post('/edit/:id', (req, res) => {
+    let product = {}
+    console.log(req.body)
+    product.name = req.body.name
+    // product.advisor = req.body.advisor
+    product.description = req.body.description
+
+    let query = { _id: req.params.id }
+    Product.updateOne(query, product, function (err) {
+        if (err) {
+            console.log(err)
+            return
+        } else {
+            res.redirect('/mainpage')
+        }
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    if (!req.user._id) {
+        res.status(500).send()
+    }
+    console.log('id:',req.params.id)
+    let query = { _id: req.params.id }
+
+    Product.findById(req.params.id, function (err, product) {
+        Product.deleteOne(query, function (err) {
+            if (err) {
+                console.error(err)
+            }
+            res.send('Success')
+        })
+    })
+})
+
+
+// Get Single Article
+router.get('/:id', (req, res) => {
+
+    Product.findById(req.params.id, function (err, product) {
+
+        User.findById(product.advisor, function (err, user) {
+            console.log(user)
+            res.render('product', {
+                product: product,
+                advisor: user
+            })
+        })
+    })
+})
+
+
 var getAuthImageUrl = (blobName) => {
     let url = blobService.getUrl(containerName, blobName)
     let sas = blobService.generateSharedAccessSignature(containerName, blobName, {
         AccessPolicy: {
             Permissions: azureStorage.BlobUtilities.SharedAccessPermissions.READ,
             Start: azureStorage.date.minutesFromNow(-15),
-            Expiry: azureStorage.date.minutesFromNow(30)
+            Expiry: azureStorage.date.minutesFromNow(3000)
         }
     })
-    console.log('sas: ',sas)
+    console.log('sas: ', sas)
 
     return `${url}?${sas}`
+}
+
+// Access Control
+var ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated) {
+        return next()
+    } else {
+        res.redirect('/users/login')
+    }
 }
 
 
