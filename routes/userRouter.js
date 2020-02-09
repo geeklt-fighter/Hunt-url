48f4968e21c1591b40f7f6326d251e8177e7d290
timlo <a0987642936@yahoo.com>
2020-02-09 17:49:59 +0800

    Create user unit function getMe deleteMe and updateMe

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const { signup, login, forgotPassword, resetPassword } = require('../controller/authController')
+const { signup, login, forgotPassword, resetPassword, protect, updatePassword } = require('../controller/authController')
+const { getMe, getUser, updateMe, deleteMe } = require('../controller/userController')
 const router = express.Router()
 
 
@@ -7,13 +8,16 @@ router.post('/signup', signup)
 router.post('/login', login)
 // router.get('/logout')
 
-
 router.post('/forgotPassword', forgotPassword)
 router.post('/resetPassword/:token', resetPassword)
 
-// router.post('/forgotPassword')
-// router.patch('/resetPassword/:token')
+// This will basically prtect all the routes come after this middleware
+router.use(protect)
 
+router.patch('/updatePassword', updatePassword)
+router.get('/me', getMe, getUser)
+router.delete('/deleteMe', deleteMe)
+router.patch('/updateMe', updateMe)
 
 
 module.exports = router
