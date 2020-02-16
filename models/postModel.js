4ad85e9fb4a73cf7c4e0917ab2691889988cee92
timlo <a0987642936@yahoo.com>
2020-02-16 15:16:31 +0800

    Add the new column in the postModel

--- models/postModel.js
+++ models/postModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const slugify = require('slugify')
 
 const postSchema = new mongoose.Schema({
     name: {
@@ -8,27 +8,52 @@ const postSchema = new mongoose.Schema({
         trim: true,
         maxlength: [50, 'A post name must have less or equal than 30 characters']
     },
+    slug: String,
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a level'],
+        enum: {
+            values: ['easy', 'medium', 'hard'],
+            message: 'Difficulty is either: easy, medium, difficult'
+        }
+    },
     theme: {
         type: String,
         required: [true, 'A post must have a theme']
     },
+    summary: {
+        type: String,
+        maxlength: 100,
+        required: [true, 'Summarize the content in your post']
+    },
     description: {
         type: String,
-        trim: true
+        trim: true,
+        required: [true, 'A post must have a description']
     },
     mediaResource: {
         type: String,
         required: [true, 'You must sharing something']
     },
+    images: [String],
     poster: {
         type: mongoose.Schema.ObjectId,
         ref: 'User'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
     }
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
 })
 
+
+postSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true })
+    next()
+})
 
 
 const Post = mongoose.model('Post', postSchema)
