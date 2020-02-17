e91bc9bed7e6eb932d377fd66d9a8c4fc9423a94
timlo <a0987642936@yahoo.com>
2020-02-17 17:01:53 +0800

    Add the upload image function and resize the image to the public folder

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ app.use('/api/v1/users', userRouter)
 app.use('/api/v1/posts', postRouter)
 
 app.all('*', (req, res, next) => {
-
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
 })
 
